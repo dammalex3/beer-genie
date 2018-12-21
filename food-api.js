26209dedac7d49cec41cc5f0e440065b4036b3d9
Riley Brown <brown.riley.c@gmail.com>
2018-12-20 21:26:50 -0500

    created the getRecipes function, takes one parameter which is the index of the food pairing item in the foodQueryArray.  Currently only logs data of interest.

--- food-api.js
+++ food-api.js
@@ -1,15 +1,22 @@
 var foodQueryArray = ["chicken", "beef", "grains", "beans", "shellfish", "pork"]
-var foodQueryURL = "https://api.edamam.com/search?" + query + "&app_id=4149b34a&app_key=3f5a1c6c3c7f31eb7143f33b706fafab";
-var app_ID = "4149b34a";
-var app_key = "3f5a1c6c3c7f31eb7143f33b706fafab";
-var query = "q=" + foodQueryArray;
-
-var getRecipes = function () {
+var counter = 0;
+var getRecipes = function (num) {
+    var query = "q=" + foodQueryArray[num];
+    var foodQueryURL = "https://api.edamam.com/search?" + query + "&app_id=4149b34a&app_key=3f5a1c6c3c7f31eb7143f33b706fafab";
+    console.log(foodQueryURL)
     $.ajax({
         url: foodQueryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        
+        var data = response.hits;
+        console.log(data.length);
+        for (var k = 0; k < data.length; k++) {
+            console.log(data[k].recipe.url);
+            console.log(data[k].recipe.ingredients);
+            console.log(data[k].recipe.image);
+        }
     });
-}
+};
+
+getRecipes(2);
