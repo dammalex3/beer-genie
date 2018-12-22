9c21c66d754c74745933663c879502ebee532abc
Riley Brown <brown.riley.c@gmail.com>
2018-12-22 13:06:23 -0500

    Got the beer api to take an ajax call, now we are retrieving a single random beer.  Maybe possible to get a list still, but need to think on it.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,33 +2,31 @@ $(document).ready(function () {
     var antiCORS = 'https://cors-anywhere.herokuapp.com/';
 
     $('.styleCard').on('click', function () {
-        
-        var style = $(this).attr('data-style');
-        style.toLowerCase().trim();
         console.log(style);
-        var beerQueryURL = antiCORS+'https://api.brewerydb.com/v2/search/style?q='+style
+        var beerQueryURL = antiCORS+'https://api.brewerydb.com/v2/beer/random?key=4b70afc53eb299a79abe53c0e1a2f954'
 
             // Here is where the ajax call for the brewerydb api will go.
             // run a get with a query for style using the style variable we grabbed from the on click
             // next fetch data from the response object to print into a table made by jquery
 
-
-        // $.ajax({
-        //     url: beerQueryURL,
-        //     method: "GET"
-        // }).then(function (response) {
-        //     console.log(response);
-        //     var data = response.path.to.data;
+        $.ajax({
+            url: beerQueryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            var data = response.data;
 
             // Here we create a table with Jquery,  Inside the for loop below we will append rows to the table 
             // Here is the for loop, this is going to fetch the  
-        //     for (var k = 0; k < data.length; k++) {
-        //         console.log(data.name);
-        //         console.log(data.details.abv);
-        //         console.log(data.details.ibus);
-        //         console.log("Ingredient Pair")
-        //     };
-        // })
+
+            // This ajax call now grabs a random beer, from this we can tack on another ajax call to get more specific info about the beer.
+            // From there we can make a table of recommended ingredients and recipes
+
+            for (var k = 0; k < data.length; k++) {
+                console.log(data[k]);
+                console.log("Ingredient Pair")
+            };
+        })
 
 
 
