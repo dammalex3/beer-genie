2c8798d686a0778d51e1d11b12fc0ee7c19dd2e7
Riley Brown <brown.riley.c@gmail.com>
2018-12-22 11:16:16 -0500

    Began adding basic bootstrap html

--- food-api.js
+++ food-api.js
@@ -2,7 +2,7 @@ var foodQueryArray = ["chicken", "beef", "grains", "beans", "shellfish", "pork"]
 var counter = 0;
 var getRecipes = function (num) {
     var query = "q=" + foodQueryArray[num];
-    var foodQueryURL = "https://api.edamam.com/search?" + query + "&app_id=4149b34a&app_key=3f5a1c6c3c7f31eb7143f33b706fafab";
+    var foodQueryURL = "https://sandbox-api.brewerydb.com/v2/search?" + query + "&app_id=4149b34a&app_key=3f5a1c6c3c7f31eb7143f33b706fafab";
     console.log(foodQueryURL)
     $.ajax({
         url: foodQueryURL,
