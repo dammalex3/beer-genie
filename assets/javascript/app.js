3b788c466c2867248438cf60c42916f45b86bbb9
Alex <adamm@urbn.com>
2018-12-26 11:01:33 -0800

    calling beer api and building the table of beers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,54 @@
 $(document).ready(function () {
+    
     var antiCORS = 'https://cors-anywhere.herokuapp.com/';
+    var numBeerResults = 5;
 
     $('.styleCard').on('click', function () {
-        console.log(style);
-        var beerQueryURL = antiCORS+'https://api.brewerydb.com/v2/beer/random?key=4b70afc53eb299a79abe53c0e1a2f954'
+        
+        //get the list of styles for that beer
+        var beerStyles = $(this).attr("data-style");
 
+        //create an array of the list of styles
+        var beerStylesArray = beerStyles.split(",");
+
+        //create the shell of the table using Jquery
+        $("#beer-list-div").empty();
+        var beerTable = $("<table>");
+        beerTable.attr('id', 'beer-table');
+        $("#beer-list-div").append(beerTable);
+        
+        //loop through the array and call the brewery db api with each style associated with the beer selected to get a random beer of that style
+        for (var i=0; i<beerStylesArray.length; i++) {
+
+            var beerQueryURL = antiCORS + 'https://api.brewerydb.com/v2/beer/random?styleId=' + beerStylesArray[i] + '&key=ca93fb5030f16f2b478658d317dc88a3';
+
+            $.ajax({
+                url: beerQueryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(JSON.stringify(response));
+                
+                //get the values from the api response
+                var beerName = response.data.nameDisplay;
+                var beerStyle = response.data.style.shortName;
+                var beerABV = response.data.abv;
+                
+                // Create the new row
+                var newRow = $("<tr>").append(
+                    $("<td>").text(beerName),
+                    $("<td>").text(beerStyle),
+                    $("<td>").text(beerABV),
+                );
+
+                $("#beer-table").append(newRow);
+    
+            })
+
+
+        }
             // Here is where the ajax call for the brewerydb api will go.
             // run a get with a query for style using the style variable we grabbed from the on click
             // next fetch data from the response object to print into a table made by jquery
-
-        $.ajax({
-            url: beerQueryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            var data = response.data;
-
-            // Here we create a table with Jquery,  Inside the for loop below we will append rows to the table 
-            // Here is the for loop, this is going to fetch the  
-
-            // This ajax call now grabs a random beer, from this we can tack on another ajax call to get more specific info about the beer.
-            // From there we can make a table of recommended ingredients and recipes
-
-            for (var k = 0; k < data.length; k++) {
-                console.log(data[k]);
-                console.log("Ingredient Pair")
-            };
-        })
 
 
 
