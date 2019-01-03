77ce19b30862f64e6f7e94a708006445a53bf6c0
Alex <adamm@urbn.com>
2019-01-02 19:09:10 -0500

    style and beer id api calls

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
     
     var antiCORS = 'https://cors-anywhere.herokuapp.com/';
-    var numBeerResults = 5;
 
     $('.styleCard').on('click', function () {
         
-        //get the list of styles for that beer
+        //get the list of styles for that beer from the html
         var beerStyles = $(this).attr("data-style");
 
         //create an array of the list of styles
@@ -14,43 +13,54 @@ $(document).ready(function () {
         //create the shell of the table using Jquery
         $("#beer-list-div").empty();
         var beerTable = $("<table>");
+        var beerTableHeader = $("<thead>");
         beerTable.attr('id', 'beer-table');
         $("#beer-list-div").append(beerTable);
         
-        //loop through the array and call the brewery db api with each style associated with the beer selected to get a random beer of that style
+        //loop through the array and call the brewery db api with each style to get a random beer of that style
         for (var i=0; i<beerStylesArray.length; i++) {
 
-            var beerQueryURL = antiCORS + 'https://api.brewerydb.com/v2/beer/random?styleId=' + beerStylesArray[i] + '&key=ca93fb5030f16f2b478658d317dc88a3';
+            var beerStyleQueryURL = antiCORS + 'https://api.brewerydb.com/v2/beer/random?styleId=' + beerStylesArray[i] + '&key=ca93fb5030f16f2b478658d317dc88a3';
 
             $.ajax({
-                url: beerQueryURL,
-                method: "GET"
-            }).then(function (response) {
-                console.log(JSON.stringify(response));
-                
-                //get the values from the api response
-                var beerName = response.data.nameDisplay;
-                var beerStyle = response.data.style.shortName;
-                var beerABV = response.data.abv;
-                
-                // Create the new row
-                var newRow = $("<tr>").append(
-                    $("<td>").text(beerName),
-                    $("<td>").text(beerStyle),
-                    $("<td>").text(beerABV),
-                );
+                url: beerStyleQueryURL,
+                method: "GET",
+                success: function (response){
+                    var beerID = response.data.id;
+                    console.log(beerID);
+                    var beerIDQueryURL = antiCORS + 'https://sandbox-api.brewerydb.com/v2/beers/?ids='+beerID+'&key=ca93fb5030f16f2b478658d317dc88a3';
+                    $.ajax({
+                        url: beerIDQueryURL,
+                        method: "GET"
+                    }).then(function (IDresponse) {
+                        
+                        // get the values from the api response
+                        var beerName = IDresponse.data[0].nameDisplay;
+                        var beerDescription = IDresponse.data[0].description;
+                        var beerABV = IDresponse.data[0].abv;
+                        var beerPicURL = IDresponse.data[0].labels.medium;
 
-                $("#beer-table").append(newRow);
+                        var beerPic = $("<img>");
+                        beerPic.attr("src", beerPicURL);
+                        // console.log(beerPic);
+                        
+                        // Create the new row
+                        var newRow = $("<tr>");
+                        var newTD = $("<td>");
+                        newTD.append(beerPic)
+                        newRow.append(newTD);
+                        newRow.append($("<td>").text(beerName));
+                        newRow.append($("<td>").text(beerABV));
+                        newRow.append($("<td>").text(beerDescription));
     
-            })
+                        $("#beer-table").append(newRow);
+                        // $("#beer-list-div").append(beerPic);
+    
+                    });
+                }
+            });
 
-
-        }
-            // Here is where the ajax call for the brewerydb api will go.
-            // run a get with a query for style using the style variable we grabbed from the on click
-            // next fetch data from the response object to print into a table made by jquery
-
-
+        }        
 
     })
 });
